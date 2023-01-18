a557a95f6aee4e3ccf54e9fe5508c5dcd4598c15
Henrywalker2002 <46373762+Henrywalker2002@users.noreply.github.com>
2023-01-18 18:13:57 +0700

    crud subject

--- back-end/webtest/src/main/java/com/example/webtest/subjectController.java
+++ back-end/webtest/src/main/java/com/example/webtest/subjectController.java
@@ -3,10 +3,8 @@ package com.example.webtest;
 import com.controller.connectDb;
 import com.model.Subject;
 import com.fasterxml.jackson.databind.JsonNode;
-
 import com.mongodb.client.MongoDatabase;
 import java.util.List;
-
 
 import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
@@ -22,15 +20,20 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import com.mongodb.BasicDBObject;
 
+
+
 @Controller
 public class subjectController {
-    @RequestMapping(value= "/getAllSubject", method = RequestMethod.GET)
-    public ResponseEntity<List<Document>> getAll() {
-        connectDb conn = new connectDb();
-        MongoDatabase db = conn.gDatabase();
 
-        MongoCollection <Document> account = db.getCollection("subject");
-        MongoCursor <Document> cursor = account.find().iterator();
+    connectDb conn = new connectDb();
+    MongoDatabase db = conn.gDatabase();
+    MongoCollection <Subject> subjCollection = db.getCollection("subject", Subject.class);
+    MongoCollection <Document> subDoc = db.getCollection("subject");
+    
+    @RequestMapping(value= "/subject", method = RequestMethod.GET)
+    public ResponseEntity<List<Document>> getAll() {
+
+        MongoCursor <Document> cursor = this.subDoc.find().iterator();
         List<Document> res = new ArrayList<Document>();
 
         while(cursor.hasNext()) {
@@ -41,7 +44,7 @@ public class subjectController {
         return new ResponseEntity<List<Document>>(res, HttpStatus.OK);
     }
 
-    @RequestMapping(value = "/addSubject", method = RequestMethod.POST)
+    @RequestMapping(value = "/subject", method = RequestMethod.POST)
     public ResponseEntity<Document> addSubject(@RequestBody JsonNode json) {
         // get data from json
         String name, teacherId, topic;
@@ -58,15 +61,11 @@ public class subjectController {
 
         int id = ThreadLocalRandom.current().nextInt(10000, 99999);
 
-        connectDb conn = new connectDb();
-        MongoDatabase db = conn.gDatabase();
-        MongoCollection <Subject> subjCollection = db.getCollection("subject", Subject.class);
-
         // if id is exist, random again
-        var temp = subjCollection.find(Filters.eq("id", id));
+        var temp = this.subjCollection.find(Filters.eq("id", id));
         while (temp.first() != null) {
             id = ThreadLocalRandom.current().nextInt(10000, 99999);
-            temp = subjCollection.find(Filters.eq("id", id));
+            temp = this.subjCollection.find(Filters.eq("id", id));
         }
 
         // check if subject and teacher is exist
@@ -76,7 +75,7 @@ public class subjectController {
         obj.add(new BasicDBObject("name", name));
         andQuery.put("$and", obj);
 
-        temp = subjCollection.find(andQuery);
+        temp = this.subjCollection.find(andQuery);
         if (temp.first() != null) {
             Document res = new Document("result", "fail");
             res.append("message", "have already exist");
@@ -84,9 +83,109 @@ public class subjectController {
         }
         Subject newSub = new Subject(id, name, teacherId, topic);
 
-        subjCollection.insertOne(newSub);
+        this.subjCollection.insertOne(newSub);
         Document res = new Document("result","ok");
         res.append("id", id);
         return new ResponseEntity<Document>(res, HttpStatus.OK);
     }
+
+    @RequestMapping(value= "/subject", method = RequestMethod.PUT)
+    public ResponseEntity<Document> updateSubject(@RequestBody JsonNode json) {
+        String name, teacherId, topic;
+        int id;
+        try {
+            name = json.get("name").textValue();
+            teacherId = json.get("teacherId").textValue();
+            topic = json.get("topic").textValue();
+            id = json.get("id").asInt();
+        }
+        catch (Exception e) {
+            Document res = new Document("result", "fail");
+            res.append("message", "wrong json");
+            return new ResponseEntity<Document>(res, HttpStatus.OK);
+        }
+        // handle exception
+        BasicDBObject query = new BasicDBObject("id", id);
+        var iterator = this.subjCollection.find(query);
+        Document res = new Document();
+        if (iterator.first() == null) {
+            res.append("result", "fail")
+                .append("message", "subject does not exist");
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        }
+
+        Subject oldSub = iterator.first();
+        if (oldSub.name != name || oldSub.teacherId != teacherId) {
+            BasicDBObject andQuery = new BasicDBObject();
+            List<BasicDBObject> obj = new ArrayList<BasicDBObject>();
+            obj.add(new BasicDBObject("teacherId", teacherId));
+            obj.add(new BasicDBObject("name", name));
+            andQuery.put("$and", obj);
+    
+            var temp = this.subjCollection.find(andQuery);
+            if (temp.first() != null) {
+                res.append("result", "fail")
+                    .append("message", "name is exist, please select other name");
+                return new ResponseEntity<Document>(res, HttpStatus.OK);
+            }
+        }
+        Subject newSub = new Subject(id, name, teacherId, topic);
+        this.subjCollection.findOneAndReplace(query, newSub);
+        res.append("result", "ok")
+            .append("message", "success");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+        
+    @RequestMapping(value = "/subject", method = RequestMethod.DELETE)
+    public ResponseEntity<Document> deleteSubject(@RequestBody JsonNode json) {
+        int id;
+        try {
+            id = json.get("id").asInt();
+        }
+        catch (Exception e) {
+            Document res = new Document("result", "fail");
+            res.append("message", "wrong json");
+            return new ResponseEntity<Document>(res, HttpStatus.OK);
+        }
+
+        BasicDBObject query = new BasicDBObject("id", id);
+        var iterator = this.subjCollection.find(query);
+        Document res = new Document();
+        if (iterator.first() == null) {
+            res.append("result", "fail")
+                .append("message", "subject does not exist");
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        }
+        this.subjCollection.findOneAndDelete(query);
+        res.append("result", "ok")
+            .append("message", "success");
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+
+    @RequestMapping(value="/getDetailSubject", method= {RequestMethod.POST, RequestMethod.GET})
+    public ResponseEntity<Document> getDetailSubject(@RequestBody(required = false) JsonNode json) {
+        int id;
+        try {
+            id = json.get("id").asInt();
+        }
+        catch (Exception e) {
+            Document res = new Document("result", "fail");
+            res.append("message", "wrong json");
+            return new ResponseEntity<Document>(res, HttpStatus.OK);
+        }
+
+        BasicDBObject query = new BasicDBObject("id", id);
+        var iterator = this.subjCollection.find(query);
+        Document res = new Document();
+        if (iterator.first() == null) {
+            res.append("result", "fail")
+                .append("message", "subject does not exist");
+            return new ResponseEntity<>(res, HttpStatus.OK);
+        }
+
+        res.append("result", "ok")
+            .append("message", iterator.first());
+        return new ResponseEntity<>(res, HttpStatus.OK);
+    }
+    
 }
--- back-end/webtest/src/main/java/com/model/Subject.java
+++ back-end/webtest/src/main/java/com/model/Subject.java
@@ -1,16 +1,33 @@
 package com.model;
 
+import org.bson.codecs.pojo.annotations.BsonProperty;
+
 public class Subject {
+    @BsonProperty(value = "id")
     public int id;
     public String name;
     public String teacherId;
     public String topic;
+
+    public Subject() {
+        this.id = 0;
+        this.name = "";
+        this.teacherId = "";
+        this.topic = "";
+    }
 
     public Subject(int id, String name, String teacherId, String topic) {
         this.id = id;
         this.name = name;
         this.teacherId = teacherId;
         this.topic = topic;
+    }
+
+    public Subject(Subject otherSub) {
+        this.id = otherSub.id;
+        this.name = otherSub.name;
+        this.teacherId = otherSub.teacherId;
+        this.topic = otherSub.topic;
     }
 
     public String toString() {
@@ -23,5 +40,13 @@ public class Subject {
     
     public int hashCode() {
         return id + name.hashCode() + teacherId.hashCode() + topic.hashCode();
+    }
+
+
+    public void assign(Subject otherSub) {
+        this.id = otherSub.id;
+        this.name = otherSub.name;
+        this.teacherId = otherSub.teacherId;
+        this.topic = otherSub.topic;
     }
 }
